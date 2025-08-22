2242631e6e5c55bbb028e5c9897d1cc80d3b02f2
MohammadReza-Hassani <mohammadrezahassani122@gmail.com>
2025-08-22 06:58:04 -0700

    add health check service

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,15 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
-import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import {NestFactory} from '@nestjs/core';
+import {AppModule} from './app.module';
+import {DocumentBuilder, SwaggerModule} from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
+  app.enableCors({
+    origin: ['http://localhost:5173'],
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  });
   // Swagger configuration
   const config = new DocumentBuilder()
       .setTitle('User Management API')
